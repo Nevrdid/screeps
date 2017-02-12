41570167b0db07713c65837915dc674339d60c4d
DESKTOP-6O5TUI0\nevrd <nevrdid@msn.com>
2017-02-12 06:52:47 +0100

    some stats changements

--- src/config_brain_memory.js
+++ src/config_brain_memory.js
@@ -1,6 +1,8 @@
 'use strict';
 
 brain.prepareMemory = function() {
+  Memory.username = Memory.username || _.find(Game.spawns, 'owner').owner.username;
+  Memory.constructionSites = Memory.constructionSites || {};
   Memory.mineralSystemPrice = {};
   Memory.ordersBuy = _.filter(Game.market.getAllOrders(), function(object) {
     if (object.type != ORDER_BUY) {
@@ -40,20 +42,11 @@ brain.prepareMemory = function() {
     Memory.constructionSites = constructionSites;
     console.log('Known constructionSites: ' + Object.keys(constructionSites).length);
   }
-
-  var userName = Memory.username || _.find(Game.spawns, 'owner').owner.username;
-  Memory.username = userName;
   // Cleanup memory
   for (let name in Memory.creeps) {
     if (!Game.creeps[name]) {
       let role = Memory.creeps[name].role;
-      if (config.stats.enabled && userName && Memory.stats && Memory.stats[userName].roles) {
-        let roleStat = Memory.stats[userName].roles[role];
-        let previousAmount = roleStat ? roleStat : 0;
-        let amount = previousAmount > 0 ? previousAmount - 1 : 0;
-        brain.stats.add(['roles', role], amount);
-      }
-
+      global.brain.modifyRoleAmount(role, -1);
       if ((name.startsWith('reserver') && Memory.creeps[name].born < (Game.time - CREEP_CLAIM_LIFE_TIME)) || Memory.creeps[name].born < (Game.time - CREEP_LIFE_TIME)) {
         delete Memory.creeps[name];
       } else {
